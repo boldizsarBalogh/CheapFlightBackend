92224ed5594a619d79b6092961e9f2afc1e506a9
boldizsarBalogh <bal.boldizsar@gmail.com>
2019-02-05 09:37:08 +0100

    Initial commit

---
+++ src/main/java/com/codecool/cheapflightapp/CheapFlightAppApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.cheapflightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapFlightAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheapFlightAppApplication.class, args);
+    }
+
+}
+
