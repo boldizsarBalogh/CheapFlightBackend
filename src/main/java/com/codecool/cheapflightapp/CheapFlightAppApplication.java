b1076d6809b9106f4f43c6274890623b523ff753
Peter Blasko <blasko.peter.hu@gmail.com>
2019-02-06 14:03:50 +0100

    get JSON to HTML with Flight's data

--- src/main/java/com/codecool/cheapflightapp/CheapFlightAppApplication.java
+++ src/main/java/com/codecool/cheapflightapp/CheapFlightAppApplication.java
@@ -1,13 +1,29 @@
 package com.codecool.cheapflightapp;
 
+import com.codecool.cheapflightapp.Service.FlightCreator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CheapFlightAppApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CheapFlightAppApplication.class);
+
+    @Autowired
+    private FlightCreator flightCreator;
+
     public static void main(String[] args) {
         SpringApplication.run(CheapFlightAppApplication.class, args);
+    }
+
+    @PostConstruct
+    public void afterInit(){
+        LOGGER.info(flightCreator.createFlight().toString());
     }
 
 }
---
+++ src/main/java/com/codecool/cheapflightapp/Controller/FlightController.java
@@ -0,0 +1,24 @@
+package com.codecool.cheapflightapp.Controller;
+
+
+import com.codecool.cheapflightapp.Service.FlightList;
+import com.codecool.cheapflightapp.model.Flight;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class FlightController {
+
+    @Autowired
+    private FlightList flightList;
+
+    @GetMapping("/")
+    public List<Flight> flightList(){
+        flightList.addFlightToFlights();
+        return flightList.getFlights();
+    }
+
+}
