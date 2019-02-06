5a411ea27dc2f76596241d4b4bd1d2803785b063
boldizsarBalogh <bal.boldizsar@gmail.com>
2019-02-06 16:47:57 +0100

    added the crossorigin annotation for the flightcontroller in order to reach the backend from the frontend

--- src/main/java/com/codecool/cheapflightapp/Controller/FlightController.java
+++ src/main/java/com/codecool/cheapflightapp/Controller/FlightController.java
@@ -4,12 +4,14 @@ package com.codecool.cheapflightapp.Controller;
 import com.codecool.cheapflightapp.Service.FlightList;
 import com.codecool.cheapflightapp.model.Flight;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class FlightController {
 
     @Autowired
