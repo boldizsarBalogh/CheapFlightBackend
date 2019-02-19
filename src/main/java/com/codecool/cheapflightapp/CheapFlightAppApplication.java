b5d831d4b4a5e65526ff3a904714d08f63a97812
boldizsarBalogh <bal.boldizsar@gmail.com>
2019-02-19 16:26:41 +0100

    refactored the flight entity to use lombok and corrected some typeos

--- src/main/java/com/codecool/cheapflightapp/CheapFlightAppApplication.java
+++ src/main/java/com/codecool/cheapflightapp/CheapFlightAppApplication.java
@@ -19,14 +19,41 @@ public class CheapFlightAppApplication {
 
     @Bean
     public void uploadDatabase(){
-        Flight flight1 = new Flight("Ryanair", "BUD", "MAD", "2019-03-01", "17:45", "20:20", "Turista", 99.9);
+        Flight flight1 = Flight.builder()
+                .company("Ryanair")
+                .startTown("BUD")
+                .arriveTown("MAD")
+                .date("2019-03-01")
+                .startTime("17:45")
+                .arriveTime("20:20")
+                .confort("Turista")
+                .price(99.9)
+                .build();
         flightRepository.save(flight1);
-        Flight flight2 = new Flight("Wizzapr", "BUD", "BER", "2019-03-02", "17:45", "20:20", "Turista", 39.95);
+        Flight flight2 = Flight.builder()
+                .company("Wizzair")
+                .startTown("BUD")
+                .arriveTown("BER")
+                .date("2019-03-02")
+                .startTime("17:45")
+                .arriveTime("20:20")
+                .confort("Turista")
+                .price(39.95)
+                .build();
+
+
         flightRepository.save(flight2);
-        Flight flight3 = new Flight("Wizzapr", "BUD", "BER", "2019-03-02", "17:45", "20:20", "Turista", 39.95);
+        Flight flight3 = Flight.builder()
+                .company("Wizzair")
+                .startTown("BUD")
+                .arriveTown("BEL")
+                .date("2019-03-03")
+                .startTime("15:45")
+                .arriveTime("16:55")
+                .confort("Turista")
+                .price(19.95)
+                .build();
         flightRepository.save(flight3);
-        Flight flight4 = new Flight("Wizzapr", "BUD", "BEL", "2019-03-03", "15:45", "16:55", "Turista", 19.95);
-        flightRepository.save(flight4);
     }
 
 }
