69ddc14ee38a87a2768d99ef69dfab50bedc4bce
Szajler Indira <szajler.indira@gmail.com>
2019-02-20 12:49:48 +0100

    getFlights() added

--- src/main/java/com/codecool/cheapflightapp/Controller/FlightController.java
+++ src/main/java/com/codecool/cheapflightapp/Controller/FlightController.java
@@ -1,14 +1,14 @@
 package com.codecool.cheapflightapp.Controller;
 
 
+import com.codecool.cheapflightapp.Repository.FlightRepository;
 import com.codecool.cheapflightapp.Service.FlightService;
 import com.codecool.cheapflightapp.model.Flight;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -16,10 +16,20 @@ public class FlightController {
 
     @Autowired
     private FlightService flightService;
+    @Autowired
+    private FlightRepository flightRepository;
 
     @GetMapping("/")
     public List<Flight> flightList(){
         return flightService.addFlightToFlights();
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/search")
+    public List<Flight> getFlights(@RequestParam Map<String, String> searchQuery) {
+        String startTown= searchQuery.get("startTown");
+        String arriveTown= searchQuery.get("arriveTown");
+        return flightRepository.findFlightByStartTownAndArriveTown(startTown, arriveTown);
+
+        }
 
 }
