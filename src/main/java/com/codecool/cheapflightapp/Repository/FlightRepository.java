14678f8db4b5fdde19c4494a685d36ca0a07caf0
Szajler Indira <szajler.indira@gmail.com>
2019-02-20 12:48:15 +0100

    findFlightByStartTownAndArriveTown added

--- src/main/java/com/codecool/cheapflightapp/Repository/FlightRepository.java
+++ src/main/java/com/codecool/cheapflightapp/Repository/FlightRepository.java
@@ -2,10 +2,14 @@ package com.codecool.cheapflightapp.Repository;
 
 import com.codecool.cheapflightapp.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FlightRepository  extends JpaRepository<Flight, Integer> {
+
+
+    List<Flight> findFlightByStartTownAndArriveTown(String start, String arrive);
 
     List<Flight> findAll();
 
