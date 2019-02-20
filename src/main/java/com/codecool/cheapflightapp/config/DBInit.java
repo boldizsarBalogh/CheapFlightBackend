f98238d111757d2328c8137db71fe59d1675ef5b
Szajler Indira <szajler.indira@gmail.com>
2019-02-20 18:29:50 +0100

    config/DBInit.java created because @DataJpaTest doesn't handle @Service

--- src/main/java/com/codecool/cheapflightapp/config/DBInit.java
+++ src/main/java/com/codecool/cheapflightapp/config/DBInit.java
@@ -1,4 +1,35 @@
 package com.codecool.cheapflightapp.config;
 
+import com.codecool.cheapflightapp.Service.DataBaseInitializer;
+import com.codecool.cheapflightapp.repository.CityRepository;
+import com.codecool.cheapflightapp.repository.FlightRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("production")
 public class DBInit {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    @Autowired
+    private DataBaseInitializer dataBaseInitializer;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Bean
+//    @Profile("production")
+    public CommandLineRunner init(){
+        return args -> {
+            String[] cityArr =  {"Budapest","Berlin","Madrid","Paris","Brussels"};
+            dataBaseInitializer.initializeCities(cityArr,cityRepository);
+            dataBaseInitializer.initalizeFlights(flightRepository,2000,cityRepository.findAll());
+        };
+    }
 }
